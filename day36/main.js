e496f7cd66b8e34c9b4aa0fd7e339750eb8f9f9f
Lowland <122749572+lowlandrepair@users.noreply.github.com>
2025-09-27 05:49:46 -0700

    update  done

---
+++ day36/main.js
@@ -0,0 +1,25 @@
+function openMenu(){
+    var links = document.getElementById("links");
+    if(links.classList.contains("show")){
+        links.classList.remove("show");
+    }else{
+        links.classList.add("show");
+    }
+}
+
+
+function search(){
+    var input = document.getElementById("search");
+    var filter = input.value.toUpperCase();
+    var ul = document.getElementById("mylist");
+    var li = ul.getElementsByTagName("li");
+
+    for (var i = 0; i < li.length; i++) {
+        var txtValue = li[i].textContent || li[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "block";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
